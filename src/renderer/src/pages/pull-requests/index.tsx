ecf5acbc7012398723cc81166ae156c26ad24298
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-11-25 21:30:58 +0900

    feat: get pull requests

--- src/renderer/src/pages/pull-requests/index.tsx
+++ src/renderer/src/pages/pull-requests/index.tsx
@@ -1,27 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useQueries } from '@tanstack/react-query'
+import { useState } from 'react'
 import { useSettingStore } from '../settings/model'
 
 export const PullRequestsPage = () => {
-  const [pulls, setPulls] = useState<any>()
   const { setting } = useSettingStore((state) => state)
 
   if (!setting) {
     return <>setting not found</>
   }
 
-  useEffect(() => {
-    window.api
-      .getPullRequests({
-        accessToken: setting.accessToken,
-        repository: 'payhereinc/payhere-pos-pc'
-      })
-      .then(setPulls)
-  }, [])
+  const queries = useQueries({
+    queries: setting.repositories?.map((repo) => ({
+      queryKey: ['pull-requests', repo],
+      queryFn: async () => {
+        const { data } = await window.api.getPullRequests({
+          accessToken: setting.accessToken,
+          repository: repo
+        })
+        return data
+      }
+    }))
+  })
 
   return (
     <>
       <div>PullRequestsPage</div>
-      <pre>{JSON.stringify(pulls, null, 2)}</pre>
+      <div>
+        {queries.map((query, i) => (
+          <div className="p-4 border" key={i}>
+            {query.data?.map((pull) => (
+              <div className="p-4 border" key={pull.id}>
+                <div>
+                  {pull.base.repo.full_name} - {pull.base.ref}
+                </div>
+                <div className="text-lg font-bold">{pull.title}</div>
+                <div>{pull.user.login}</div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   )
 }
